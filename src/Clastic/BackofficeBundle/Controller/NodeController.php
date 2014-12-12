29cd03760a4ac14f827727363c80e1b47d783701
Dries De Peuter <dries@nousefreak.be>
2014-12-12 13:12:00 +0100

    Add delete route.

--- src/Clastic/BackofficeBundle/Controller/NodeController.php
+++ src/Clastic/BackofficeBundle/Controller/NodeController.php
@@ -9,10 +9,12 @@
 
 namespace Clastic\BackofficeBundle\Controller;
 
+use Clastic\BackofficeBundle\Form\NodeDeleteType;
 use Clastic\CoreBundle\Entity\Node;
 use Clastic\BackofficeBundle\Form\NodeType;
 use Clastic\CoreBundle\Module\ModuleManager;
 use Clastic\TextBundle\Entity\Text;
+use Doctrine\ORM\EntityRepository;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -31,7 +33,7 @@ class NodeController extends Controller
             ->getManager()
             ->createQueryBuilder()
             ->select('e')
-            ->from('ClasticTextBundle:Text', 'e')
+            ->from($this->getEntityName($type), 'e')
             ->orderBy('e.id', 'DESC')
         ;
 
@@ -44,12 +46,12 @@ class NodeController extends Controller
         ));
     }
 
-    public function formAction($type, $node_id, Request $request)
+    public function formAction($type, $nodeId, Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
-        if (!is_null($node_id)) {
-            $data = $em->getRepository('ClasticTextBundle:Text')->find($node_id);
+        if (!is_null($nodeId)) {
+            $data = $this->getRepository($type)->find($nodeId);
         } else {
             $data = new Text();
 
@@ -87,10 +89,42 @@ class NodeController extends Controller
             )));
         }
 
-
         return $this->render('ClasticBackofficeBundle:Node:form.html.twig', array(
             'form' => $form->createView(),
         ));
+    }
+
+    public function deleteAction($type, $nodeId)
+    {
+        $data = $this->getRepository($type)->find($nodeId);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($data);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('clastic_backoffice_list', array(
+            'type' => $type,
+        )));
+    }
+
+    /**
+     * @param string $type
+     * @return EntityRepository
+     */
+    private function getRepository($type)
+    {
+        return $this->getDoctrine()->getManager()
+            ->getRepository($this->getEntityName($type));
+    }
+
+    /**
+     * @param string $type
+     * @return string
+     */
+    private function getEntityName($type)
+    {
+        return 'ClasticTextBundle:Text';
     }
 
     /**
