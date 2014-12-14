dd8000c3d2258788ea5d0f239bda9fb627baf134
Dries De Peuter <dries@nousefreak.be>
2014-12-15 00:31:24 +0100

    [Alias] Alter node form.

---
+++ src/Clastic/AliasBundle/Form/Extension/NodeTypeExtension.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\AliasBundle\Form\Extension;
+
+use Clastic\BackofficeBundle\Event\NodeFormBuildEvent;
+use Clastic\BackofficeBundle\NodeFormEvents;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * NodeTypeExtension
+ *
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class NodeTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * Returns the name of the type being extended.
+     *
+     * @return string The name of the type being extended
+     */
+    public function getExtendedType()
+    {
+        return 'clastic_node';
+    }
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->get('tabs')
+            ->add($this->createTab($builder, 'alias', array('label' => 'Alias')));
+    }
+
+    private function createTab(FormBuilderInterface $builder, $name, $options = array())
+    {
+        $options = array_replace(
+            $options,
+            array(
+                'inherit_data' => true,
+            ));
+
+        return $builder->create($name, 'tabs_tab', $options);
+    }
+
+}
