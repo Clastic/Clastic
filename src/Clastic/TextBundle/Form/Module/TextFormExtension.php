4782e1ee3565619c4e9e7b7a1e62a53208211dc4
Dries De Peuter <dries@nousefreak.be>
2014-12-14 23:57:09 +0100

    [Text] Node form.

---
+++ src/Clastic/TextBundle/Form/Module/TextFormExtension.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\TextBundle\Form\Module;
+
+use Clastic\BackofficeBundle\Form\Extension\AbstractNodeTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * NodeTypeExtension
+ *
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class TextFormExtension extends AbstractNodeTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->findTab($builder, 'general')
+            ->add('body', 'textarea');
+    }
+}
