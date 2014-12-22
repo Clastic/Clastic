9feb8c06544ddc7a0a493d6fc034f961d823eede
Dries De Peuter <dries@nousefreak.be>
2014-12-22 23:23:50 +0100

    [User] Init module

---
+++ src/Clastic/UserBundle/Entity/User.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class User extends BaseUser
+{
+}
