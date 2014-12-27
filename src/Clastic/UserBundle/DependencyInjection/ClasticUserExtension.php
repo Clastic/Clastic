6de00c0b5099c02169dcea844cd77f1db12c7091
Dries De Peuter <dries@nousefreak.be>
2014-12-27 18:48:03 +0100

    [User] Move config to bundle.

--- src/Clastic/UserBundle/DependencyInjection/ClasticUserExtension.php
+++ src/Clastic/UserBundle/DependencyInjection/ClasticUserExtension.php
@@ -4,6 +4,7 @@ namespace Clastic\UserBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ClasticUserExtension extends Extension
+class ClasticUserExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +25,42 @@ class ClasticUserExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (true === isset($bundles['FOSUserBundle'])) {
+            $this->configureFOSUserBundle($container);
+        }
+    }
+
+    /**
+     * @param ContainerBuilder $container The service container
+     *
+     * @return void
+     */
+    protected function configureFOSUserBundle(ContainerBuilder $container)
+    {
+        foreach (array_keys($container->getExtensions()) as $name) {
+            switch ($name) {
+                case 'fos_user':
+                    $container->prependExtensionConfig(
+                        $name,
+                        array(
+                            'db_driver' => 'orm',
+                            'firewall_name' => 'main',
+                            'user_class' => 'Clastic\UserBundle\Entity\User',
+                        )
+                    );
+                    break;
+            }
+        }
     }
 }
