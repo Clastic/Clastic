799b1797992584eee5bb0966fba1f2cc2690ec65
Dries De Peuter <dries@nousefreak.be>
2014-12-30 01:21:34 +0100

    [User] Add admin user fixture.

---
+++ src/Clastic/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\UserBundle\DataFixtures\ORM;
+
+use Clastic\UserBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setSalt(md5(uniqid()));
+
+        $encoder = $this->container
+            ->get('security.encoder_factory')
+            ->getEncoder($userAdmin)
+        ;
+        $userAdmin->setPassword($encoder->encodePassword('secret', $userAdmin->getSalt()));
+
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+
+        $this->addReference('admin-user', $userAdmin);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
