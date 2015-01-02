7b89adc92d804b250b2a27ba91e9368d20c45075
Dries De Peuter <dries@nousefreak.be>
2015-01-03 00:30:34 +0100

    [Backoffice] Add login form test

--- src/Clastic/BackofficeBundle/Tests/SecurityTest.php
+++ src/Clastic/BackofficeBundle/Tests/SecurityTest.php
@@ -24,4 +24,16 @@ class SecurityTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isRedirect());
         $this->assertRegExp('|/admin/login$|', $client->getResponse()->headers->get('location'));
     }
+
+    public function testLoginForm()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/admin/login');
+
+        $this->assertEquals(1, $crawler->filter('form')->count());
+        $this->assertEquals(1, $crawler->filter('form input[name="_username"]')->count());
+        $this->assertEquals(1, $crawler->filter('form input[name="_password"]')->count());
+        $this->assertEquals(1, $crawler->filter('form input[name="_remember_me"]')->count());
+        $this->assertEquals(1, $crawler->filter('form button[name="_submit"]')->count());
+    }
 }
