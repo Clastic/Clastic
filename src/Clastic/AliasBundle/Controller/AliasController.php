0cc4ec78c191ffde581110730cb1dbc023784fad
Dries De Peuter <dries@nousefreak.be>
2015-01-03 02:47:25 +0100

    [Alias] Abstract Module Controllers.

--- src/Clastic/AliasBundle/Controller/AliasController.php
+++ src/Clastic/AliasBundle/Controller/AliasController.php
@@ -11,83 +11,69 @@ namespace Clastic\AliasBundle\Controller;
 
 use Clastic\AliasBundle\Entity\Alias;
 use Clastic\AliasBundle\Form\AliasType;
+use Clastic\BackofficeBundle\Controller\AbstractModuleController;
 use Clastic\CoreBundle\Entity\Node;
-use Clastic\CoreBundle\Node\NodeManager;
 use Clastic\CoreBundle\Node\NodeReferenceInterface;
-use Doctrine\ORM\EntityRepository;
-use Pagerfanta\Adapter\DoctrineORMAdapter;
-use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Form;
 
 /**
  * NodeController
  *
  * @author Dries De Peuter <dries@nousefreak.be>
  */
-class AliasController extends Controller
+class AliasController extends AbstractModuleController
 {
     /**
-     * @return Response
+     * @return string
      */
-    public function listAction()
+    protected function getType()
     {
-        $queryBuilder = $this->getDoctrine()
-            ->getManager()
-            ->createQueryBuilder()
-            ->select('e')
-            ->from('ClasticAliasBundle:Alias', 'e')
-            ->orderBy('e.id', 'DESC');
-
-        $adapter = new DoctrineORMAdapter($queryBuilder);
-        $data = new Pagerfanta($adapter);
-
-        return $this->render('ClasticAliasBundle:Backoffice:list.html.twig', array(
-            'data' => $data,
-            'type' => 'alias',
-            'module' => $this->get('clastic.module_manager')->getModule('alias'),
-        ));
+        return 'alias';
     }
 
     /**
-     * @param int|null $id
-     * @param Request  $request
+     * @return string
+     */
+    protected function getListTemplate()
+    {
+        return 'ClasticAliasBundle:Backoffice:list.html.twig';
+    }
+
+    /**
+     * @param object $data
      *
-     * @return RedirectResponse|Response
+     * @return Form
      */
-    public function formAction($id, Request $request)
+    protected function buildForm($data)
     {
-        $data = $this->resolveData($id);
-        $form = $this->createForm(new AliasType(), $data);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $data = $form->getData();
-            $this->persistData($data);
-
-            $request->getSession()
-                ->getFlashBag()
-                ->add('success', 'Your changes were saved!');
-
-            return $this->redirect($this->generateUrl('clastic_backoffice_form', array(
-                'type' => 'alias',
-                'nodeId' => $data->getNode()->getId(),
-            )));
-        }
-
-        return $this->render('ClasticBackofficeBundle:Node:form.html.twig', array(
-            'form' => $form->createView(),
-            'module' => $this->get('clastic.module_manager')->getModule('alias'),
-        ));
+        return $this->createForm(new AliasType(), $data);
     }
+
     /**
-     * @param int    $id
+     * @param object $data
+     *
+     * @return string
+     */
+    protected function resolveDataTitle($data)
+    {
+        return $data->getNode()->getTitle();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getEntityName()
+    {
+        return 'ClasticAliasBundle:Alias';
+    }
+
+    /**
+     * @param int $id
      *
      * @return NodeReferenceInterface
      */
-    private function resolveData($id)
+    protected function resolveData($id)
     {
         if (!is_null($id)) {
             return $this->getDoctrine()->getRepository('ClasticAliasBundle:Alias')
@@ -95,14 +81,5 @@ class AliasController extends Controller
         }
 
         return new Alias();
-    }
-    /**
-     * @param Alias $data
-     */
-    private function persistData($data)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($data);
-        $em->flush();
     }
 }
