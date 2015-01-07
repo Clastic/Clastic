f0c817d276ccb9d4d32886a832f8c187ce0c469f
Dries De Peuter <dries.depeuter@saga.be>
2015-01-07 13:36:05 +0100

    [Menu] Add doctrine extensions.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,7 +31,9 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Clastic\NodeBundle\ClasticNodeBundle(),
+
             new Clastic\MenuBundle\ClasticMenuBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
