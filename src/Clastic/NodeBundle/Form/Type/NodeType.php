9d3f8486e33a017f3bfb1fd97cb4708ca0ed6b75
Dries De Peuter <dries@nousefreak.be>
2015-01-10 13:17:21 +0100

    [Node] Add Node selection field.

---
+++ src/Clastic/NodeBundle/Form/Type/NodeType.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\NodeBundle\Form\Type;
+
+use Clastic\MenuBundle\Entity\MenuItem;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * MultiSelectType
+ *
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class NodeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'class' => 'ClasticNodeBundle:Node',
+            'property' => 'title',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'entity';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'node';
+    }
+}
