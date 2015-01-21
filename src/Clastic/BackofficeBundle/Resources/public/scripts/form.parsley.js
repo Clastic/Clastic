5c0a8630512a4a417ef69c6bba917dcaf05b888d
Dries De Peuter <dries.depeuter@saga.be>
2015-01-21 13:28:20 +0100

    [Backoffice] Fix show of last tab when validating valid form.
     Fixes #50.

--- src/Clastic/BackofficeBundle/Resources/public/scripts/form.parsley.js
+++ src/Clastic/BackofficeBundle/Resources/public/scripts/form.parsley.js
@@ -5,7 +5,7 @@
         var $tabpanel = $form.find('.tabpanel');
         if ($tabpanel.length) {
             var $firstError = $tabpanel.find('.parsley-error').first();
-            if ($firstError) {
+            if ($firstError.length) {
                 var $tab = $form.find('[role="tablist"] li').eq($firstError.closest('.tab-pane').index());
                 $tab.find('a').tab('show');
             }
