3b1424a3f79e2da789e5550e4a870f2e186e0f12
Dries De Peuter <dries@nousefreak.be>
2015-02-07 18:15:01 +0100

    [Backoffice] Only load ckeditor when needed.

--- src/Clastic/BackofficeBundle/Resources/public/scripts/form.parsley.js
+++ src/Clastic/BackofficeBundle/Resources/public/scripts/form.parsley.js
@@ -1,14 +1,23 @@
 (function() {
-    var $form = $('form');
-    $form.parsley();
-    $.listen('parsley:form:validated', function() {
-        var $tabpanel = $form.find('.tabpanel');
-        if ($tabpanel.length) {
-            var $firstError = $tabpanel.find('.parsley-error').first();
-            if ($firstError.length) {
-                var $tab = $form.find('[role="tablist"] li').eq($firstError.closest('.tab-pane').index());
-                $tab.find('a').tab('show');
-            }
+    'use strict';
+
+    $(function() {
+        var $form = $('form');
+        if (!$form.length) {
+            return;
         }
+
+        $form.parsley();
+        $.listen('parsley:form:validated', function() {
+            var $tabpanel = $form.find('.tabpanel');
+            if ($tabpanel.length) {
+                var $firstError = $tabpanel.find('.parsley-error').first();
+                if ($firstError.length) {
+                    var $tab = $form.find('[role="tablist"] li').eq($firstError.closest('.tab-pane').index());
+                    $tab.find('a').tab('show');
+                }
+            }
+        });
     });
 }());
+
--- src/Clastic/BackofficeBundle/Resources/public/scripts/form.wysiwyg.js
+++ src/Clastic/BackofficeBundle/Resources/public/scripts/form.wysiwyg.js
@@ -2,6 +2,23 @@
     'use strict';
 
     $(function() {
-        $('textarea.wysiwyg').ckeditor();
+        var $elements = $('textarea.wysiwyg');
+        if (!$elements.length) {
+            return;
+        }
+
+        $.ajax({
+            url: "/vendor/ckeditor/ckeditor.js",
+            dataType: "script",
+            cache: true
+        }).done(function() {
+            $.ajax({
+                url: "/vendor/ckeditor/adapters/jquery.js",
+                dataType: "script",
+                cache: true
+            }).done(function() {
+                $elements.ckeditor();
+            });
+        });
     });
 }());
