ecae9dea19ab811a367e4a7bfb0380a651e60776
Dries De Peuter <dries@nousefreak.be>
2015-02-26 23:29:28 +0100

    [Backoffice] Create fieldset form element.

---
+++ src/Clastic/BackofficeBundle/Form/Type/FieldsetType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\BackofficeBundle\Form\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class FieldsetType extends FormType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'legend' => '',
+            'virtual' => true,
+        ));
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (false !== $options['legend']) {
+            $view->vars['legend'] = $options['legend'];
+        }
+        $view->vars['label'] = false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'fieldset';
+    }
+}
