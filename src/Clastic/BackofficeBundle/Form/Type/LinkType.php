4d37ebd2c5d51cad25e93a725f4428c56fcc91c1
Dries De Peuter <dries@nousefreak.be>
2015-03-01 01:03:00 +0100

    [Backoffice] Add link field.

---
+++ src/Clastic/BackofficeBundle/Form/Type/LinkType.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\BackofficeBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * MultiSelectType
+ *
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class LinkType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('label', 'text', array('required' => false))
+            ->add('link', 'text', array('required' => false));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return 'form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'link';
+    }
+}
