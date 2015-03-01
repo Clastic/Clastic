39f4649fa4e3e9aaff5124d6eb1c882835268ce9
Dries De Peuter <dries@nousefreak.be>
2015-03-01 01:15:25 +0100

    [Blog] Remove test code.

--- src/Clastic/BlogBundle/Form/Module/BlogFormExtension.php
+++ src/Clastic/BlogBundle/Form/Module/BlogFormExtension.php
@@ -37,19 +37,5 @@ class BlogFormExtension extends AbstractNodeTypeExtension
                         'required' => false,
                     ))
             );
-
-        $builder->get('tabs')->add($this
-            ->createTab($builder, 'links', array(
-                'label' => 'Links',
-                'position' => 'first',
-            ))
-            ->add('links', 'collection', array(
-                'type' => 'link',
-                'allow_add' => true,
-                'allow_delete' => true,
-                'required' => false,
-                'mapped' => false,
-            ))
-        );
     }
 }
