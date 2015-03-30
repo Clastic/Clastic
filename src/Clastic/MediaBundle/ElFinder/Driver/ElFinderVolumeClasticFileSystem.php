6fa70cf2d311cd05681b282828c12dff8ce120c3
Dries De Peuter <dries@nousefreak.be>
2015-03-31 00:13:13 +0200

    [Media] Allow place to alter behaviour.

---
+++ src/Clastic/MediaBundle/ElFinder/Driver/ElFinderVolumeClasticFileSystem.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the Clastic package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Clastic\MediaBundle\ElFinder\Driver;
+
+use FM\ElFinderPHP\Driver\ElFinderVolumeLocalFileSystem;
+
+/**
+ * ElFinderVolumeClasticFileSystem
+ *
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class ElFinderVolumeClasticFileSystem extends ElFinderVolumeLocalFileSystem
+{
+}
