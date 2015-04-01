384df7a5651e7e5980851241cc0693f1564f1656
Dries De Peuter <dries@nousefreak.be>
2015-04-01 20:42:31 +0200

    [Menu] Move configuration to Bundle Extension.

--- src/Clastic/MenuBundle/DependencyInjection/ClasticMenuExtension.php
+++ src/Clastic/MenuBundle/DependencyInjection/ClasticMenuExtension.php
@@ -4,6 +4,7 @@ namespace Clastic\MenuBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ClasticMenuExtension extends Extension
+class ClasticMenuExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +25,38 @@ class ClasticMenuExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (true === isset($bundles['StofDoctrineExtensionsBundle'])) {
+            $this->configureStofDoctrineExtensionsBundle($container);
+        }
+    }
+
+    /**
+     * @param ContainerBuilder $container The service container
+     *
+     * @return void
+     */
+    protected function configureStofDoctrineExtensionsBundle(ContainerBuilder $container)
+    {
+        foreach (array_keys($container->getExtensions()) as $name) {
+            switch ($name) {
+                case 'stof_doctrine_extensions':
+                    $container->prependExtensionConfig(
+                        $name,
+                        array('orm' => array('default' => array('tree' => true)))
+                    );
+                    break;
+            }
+        }
     }
 }
