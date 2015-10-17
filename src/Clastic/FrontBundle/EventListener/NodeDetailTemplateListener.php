fb3956ebd750d55d345726c0e6eff91de63b8d02
Dries De Peuter <dries@nousefreak.be>
2015-10-17 02:37:53 +0200

    [Front] Run CS fixer.

--- src/Clastic/FrontBundle/EventListener/NodeDetailTemplateListener.php
+++ src/Clastic/FrontBundle/EventListener/NodeDetailTemplateListener.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Clastic package.
  *
@@ -6,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Clastic\FrontBundle\EventListener;
 
 use Clastic\CoreBundle\Module\ModuleManager;
@@ -15,7 +15,7 @@ use Clastic\NodeBundle\Module\NodeModuleInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * NodeListener
+ * NodeListener.
  *
  * @author Dries De Peuter <dries@nousefreak.be>
  */
