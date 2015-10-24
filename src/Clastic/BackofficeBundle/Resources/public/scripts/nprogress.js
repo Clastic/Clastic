e9d547375e4c34cb88f828fa7db7c8b1bda4f103
Dries De Peuter <dries@nousefreak.be>
2015-10-24 02:44:06 +0200

    [Backoffice] Remove debug info.

--- src/Clastic/BackofficeBundle/Resources/public/scripts/nprogress.js
+++ src/Clastic/BackofficeBundle/Resources/public/scripts/nprogress.js
@@ -5,12 +5,11 @@
 
     var progress = window.setInterval(function() {
         NProgress.inc(0.1);
-        console.log('loading');
     }, 250);
 
-    $(window).on('load', function() {
+    $(window).load(function() {
         clearInterval(progress);
         NProgress.done();
-    })
+    });
 })();
 
