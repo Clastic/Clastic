ec845f11753ad37b2adca30f7ea94117b1097978
Dries De Peuter <dries@nousefreak.be>
2015-12-14 21:50:07 +0100

    Upgrade to sf 3.0

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -8,6 +8,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
+            // Standard symfony
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
@@ -17,34 +18,32 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
+            // Clastic Bundles
             new Clastic\CoreBundle\ClasticCoreBundle(),
-
+                new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Clastic\BackofficeBundle\ClasticBackofficeBundle(),
-            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
-            new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
-            new Ivory\OrderedFormBundle\IvoryOrderedFormBundle(),
-
+                new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+                new WhiteOctober\BreadcrumbsBundle\WhiteOctoberBreadcrumbsBundle(),
+                new Ivory\OrderedFormBundle\IvoryOrderedFormBundle(),
             new Clastic\AliasBundle\ClasticAliasBundle(),
-
             new Clastic\TextBundle\ClasticTextBundle(),
-
-            new FOS\UserBundle\FOSUserBundle(),
             new Clastic\UserBundle\ClasticUserBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+                new FOS\UserBundle\FOSUserBundle(),
             new Clastic\NodeBundle\ClasticNodeBundle(),
-
             new Clastic\MenuBundle\ClasticMenuBundle(),
-            new FM\ElfinderBundle\FMElfinderBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Demo\Bundle\DemoBundle(),
-            new Clastic\FrontBundle\ClasticFrontBundle(),
+                new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Clastic\MediaBundle\ClasticMediaBundle(),
+                new FM\ElfinderBundle\FMElfinderBundle(),
             new Clastic\TaxonomyBundle\ClasticTaxonomyBundle(),
             new Clastic\BlockBundle\ClasticBlockBundle(),
+            new Clastic\FrontBundle\ClasticFrontBundle(),
             new Clastic\SecurityBundle\ClasticSecurityBundle(),
+
+            new Demo\Bundle\DemoBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
@@ -54,8 +53,23 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
